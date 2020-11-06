3c4fa30707f50c57419d7a391414947a77757138
Paul Gaumer <mitsudomoe8@gmail.com>
2020-11-06 14:34:40 +0100

    Feature: add Tailwind CSS config

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import './App.css';
-import Home from "./components/Home"
-import About from "./components/About"
-import Posts from "./components/Posts"
-import PostsLists from "./components/PostsLists"
-import Post from "./components/Post"
+import Home from "./Home"
+import About from "./About"
+import Posts from "./Posts"
+import PostsLists from "./PostsLists"
+import Post from "./Post"
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
